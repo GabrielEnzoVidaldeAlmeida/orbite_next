bb58ed63128b5b8205cb618c12b1ad103a50a8e7
Gabriel Enzo <111479378+GabrielEnzoVidaldeAlmeida@users.noreply.github.com>
2025-05-12 16:20:45 -0300

    versao mobile login page completa

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,27 +1,51 @@
 import RegisterInput from "@/components/RegisterInput";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function LoginPage() {
   return (
-    <div>
-      <div>
-        <Image src="/images/logo.png" alt="logo" width={1280} height={720} />
+    <div className="flex flex-col h-screen px-6">
+      <div className="h-1/2">
+        <Image
+          src="/images/logo.png"
+          alt="logo"
+          width={1280}
+          height={720}
+          priority
+        />
       </div>
 
-      <div>
-        <form className="flex flex-col px-6 gap-6">
+      <div className="h-1/2">
+        <form className="flex flex-col  gap-6">
           <RegisterInput
             labelInput="Nome:"
             type="text"
             placeholder="digite seu nome..."
           />
-
           <RegisterInput
             labelInput="Senha:"
             type="password"
-            placeholder="digite seu nome..."
+            placeholder="digite sua senha..."
           />
+          {/* <button className="text-orbite-ct-text-light hover:brightness-75 transition font-bold text-sm">
+            EXIBIR
+          </button> */}
+          <Link
+            href="/"
+            className="bg-link-login text-orbite-pt-text-dark font-bold rounded h-10 w-24 flex items-center justify-center self-center mt-4 text-sm hover:brightness-75 transition"
+          >
+            Login
+          </Link>
         </form>
+        <div className="flex gap-1 mt-6 justify-center font-bold">
+          <p className="text-orbite-pt-text-dark">Esqueceu a senha? </p>{" "}
+          <Link
+            href="/register"
+            className="text-link-login hover:text-orbite-caret-dark transition"
+          >
+            Criar conta
+          </Link>
+        </div>
       </div>
     </div>
   );
--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -93,9 +93,11 @@ export function NavBar() {
           <button onClick={() => setSearchBar(true)}>
             <Search className="w-5 h-5 sm:w-6 sm:h-6 cursor-pointer" />
           </button>
-          <strong className="flex items-center gap-2 text-sm sm:text-xl">
-            Entrar
-          </strong>
+          <Link href={linkLogin}>
+            <strong className="flex items-center gap-2 text-sm sm:text-xl">
+              Entrar
+            </strong>
+          </Link>
         </div>
       )}
 
